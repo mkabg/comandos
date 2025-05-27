7bb401dc1b50eb4d1e5970e35bb64723559e5852
mkabg <m7110645k@gmail.com>
2025-05-27 11:01:16 +0300

    add class weapon

---
+++ comandos/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comandos
+{
+    internal class Weapon
+    {
+        public string Name, Manufacturer;
+        public int NumAmmo;
+
+        public Weapon(string name, string manufacturer, int numAmmo)
+        {
+            this.Name = name;
+            this.Manufacturer = manufacturer;
+            this.NumAmmo = numAmmo;
+        }
+
+        public void Shoot()
+        {
+            if (NumAmmo > 0)
+            {
+                Console.WriteLine("shoting");
+                NumAmmo -= 1;
+            }
+            else
+            {
+                Console.WriteLine("there is no bullet");
+            }
+        }
+
+    }
+}
