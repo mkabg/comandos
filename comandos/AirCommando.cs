19631822fe46440400a1a970c4914280e213a418
mkabg <m7110645k@gmail.com>
2025-05-27 12:01:51 +0300

    add class air commando

---
+++ comandos/AirCommando.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comandos
+{
+    internal class AirCommando : Commando
+    {
+        
+        private string UnitName;
+
+        public AirCommando(string name, string codeName, string status, string unitName) :base(name, codeName, status)
+        {
+            this.UnitName = unitName;
+        }
+
+        public void Parachuting()
+        {
+            Console.WriteLine($"the {CodeName} is parachuting");
+            this.Status = "parachuting";
+        }
+    }
+}
